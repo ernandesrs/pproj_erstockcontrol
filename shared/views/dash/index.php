41540e313cd96fc84057b6ad92b9c72bcf3bf2ff
Ernandes R Souza <ernandesrsouza@gmail.com>
2022-06-20 14:39:54 -0400

    Dashboard

--- shared/views/dash/index.php
+++ shared/views/dash/index.php
@@ -2,8 +2,63 @@
 
 <?= $v->start("content") ?>
 
-<div class="container">
-    <h1>Visão geral</h1>
+<div class="section">
+    <div class="section-header">
+        <div class="left-side">
+            <h2 class="title">Lorem ipsum title</h2>
+        </div>
+        <div class="right-side">
+            <button class="btn btn-info">
+                Novo
+            </button>
+        </div>
+    </div>
+
+    <div class="section-content">
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio soluta non dignissimos consequatur alias facere sed amet atque pariatur, praesentium commodi eligendi at ducimus animi culpa odio, tempore consectetur nisi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis molestias repudiandae autem quae obcaecati inventore doloremque, sed corporis possimus illum sunt animi. Aperiam, eum neque harum iste tenetur officiis voluptate!
+    </div>
+</div>
+
+<div class="row justify-content-center">
+    <div class="col-12 col-lg-6">
+        <div class="section">
+            <div class="section-header">
+                <div class="left-side">
+                    <h2 class="title">Lorem ipsum title</h2>
+                </div>
+                <div class="right-side">
+                    <button class="btn btn-info">
+                        Ver lista
+                    </button>
+                </div>
+            </div>
+
+            <div class="section-content">
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio soluta non dignissimos consequatur alias facere sed amet atque pariatur, praesentium commodi eligendi at ducimus animi culpa odio, tempore consectetur nisi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis molestias repudiandae autem quae obcaecati inventore doloremque, sed corporis possimus illum sunt animi. Aperiam, eum neque harum iste tenetur officiis voluptate!
+            </div>
+        </div>
+    </div>
+    <div class="col-12 col-lg-6">
+        <div class="section">
+            <div class="section-header">
+                <div class="left-side">
+                    <h2 class="title">Lorem ipsum title</h2>
+                </div>
+                <div class="right-side">
+                    <button class="btn btn-link">
+                        Ver outra lista
+                    </button>
+                </div>
+            </div>
+
+            <div class="section-content">
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio soluta non dignissimos consequatur alias facere sed amet atque pariatur, praesentium commodi eligendi at ducimus animi culpa odio, tempore consectetur nisi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis molestias repudiandae autem quae obcaecati inventore doloremque, sed corporis possimus illum sunt animi. Aperiam, eum neque harum iste tenetur officiis voluptate!
+            </div>
+        </div>
+    </div>
 </div>
 
 <?= $v->end("content") ?>
--- shared/views/layouts/dash.php
+++ shared/views/layouts/dash.php
@@ -15,8 +15,8 @@
     <?php
 
     $styles = [
-        "/dash/custom.css",
-        "/bootstrap-icons.css"
+        "dash/custom.css",
+        "bootstrap-icons.css"
     ];
 
     foreach ($styles as $style) {
@@ -28,23 +28,70 @@
     <?= $v->section("styles") ?>
 </head>
 
-<body>
-    <div class="main-wrapp">
-        <aside class="sidebar">
-        </aside>
+<body class="w-sidebar">
+    <aside class="sidebar">
+        <div class="container-fluid">
+            <div class="profile d-flex align-items-center">
+                <img class="photo img-thumbnail rounded-circle" src="https://via.placeholder.com/45x45" alt="">
+                <div class="info pl-2 w-100">
+                    <div class="mb-0 username">Ernandes</div>
+                    <div class="mb-0 d-flex">
+                        <span class="level">
+                            <small>Proprietário</small>
+                        </span>
+                        <a class="profile-link ml-auto" href="">Sair</a>
+                    </div>
+                </div>
+            </div>
 
-        <main class="main">
+            <div class="sidebar-elements">
+                <div class="element">
+                    <h5 class="title">menu</h5>
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Resumo geral</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Produtos</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="element">
+                    <h5 class="title">outros</h5>
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Configurações</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Perfil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= url("auth/logout") ?>" title="Encerrar minha sessão">Sair</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </aside>
+
+    <main class="main">
+        <div class="container-fluid">
+            <div class="main-bar d-flex align-items-center py-2">
+                <button class="btn btn-sidebar-toggler jsSidebarToggler bi bi-arrow-left-square" data-icon-toggle="bi bi-arrow-right-square"></button>
+            </div>
+
             <?= $v->section("content") ?>
-        </main>
-    </div>
+        </div>
+    </main>
 
     <?php
 
     $scripts = [
-        "/jquery.min.js",
-        "/jquery-ui.min.js",
-        "/bootstrap.min.js",
-        "/dash/scripts.js",
+        "jquery.min.js",
+        "jquery-ui.min.js",
+        "bootstrap.min.js",
+        "dash/scripts.js",
     ];
 
     foreach ($scripts as $script) {
