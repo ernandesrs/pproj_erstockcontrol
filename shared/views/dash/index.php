ff1dbffa1f6092031ab083f2257064410e4c749b
Ernandes R Souza <ernandesrsouza@gmail.com>
2022-06-24 09:23:46 -0400

    Iniciando o Dashaboard - Overview com dados reais existentes

--- shared/views/dash/index.php
+++ shared/views/dash/index.php
@@ -2,62 +2,67 @@
 
 <?= $v->start("content") ?>
 
-<div class="section">
+<div class="section section-overview">
     <div class="section-header">
         <div class="left-side">
-            <h2 class="title">Lorem ipsum title</h2>
+            <h2 class="title">
+                Resumo geral
+            </h2>
         </div>
+    </div>
+
+    <div class="section-content border-0 bg-transparent">
+        <div class="row justify-content-center overview-boxes-list">
+            <?php if (($overviewBoxes ?? null)) :
+                foreach ($overviewBoxes as $boxe) : ?>
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                        <div class="card card-body px-3 py-3 border-0 shadow d-flex flex-row align-items-center overview-boxes-item">
+                            <i class="<?= $boxe->icon ?>"></i>
+                            <div class="w-100">
+                                <h5 class="mb-0 title">
+                                    <?= $boxe->text ?>
+                                </h5>
+                                <div class="d-flex align-items-center">
+                                    <p class="mb-0 total">
+                                        <span>Total:</span> <span class="value"><?= $boxe->total ?></span>
+                                    </p>
+                                    <a class="btn btn-sm btn-link ml-auto" href="<?= $boxe->link ?>">
+                                        Acessar
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            <?php endforeach;
+            endif; ?>
+        </div>
+    </div>
+</div>
+
+<div class="section section-overview">
+    <div class="section-header">
+        <div class="left-side">
+            <h2 class="title">
+                Lugar bom para um gráfico
+            </h2>
+            <p class="subtitle">
+                Poderia ter um gráfico maneiro aqui embaixo
+            </p>
+        </div>
+
         <div class="right-side">
-            <button class="btn btn-info">
-                Novo
-            </button>
+            <a class="btn btn-info" href="">
+                Mais detalhes
+            </a>
         </div>
     </div>
 
     <div class="section-content">
-        <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio soluta non dignissimos consequatur alias facere sed amet atque pariatur, praesentium commodi eligendi at ducimus animi culpa odio, tempore consectetur nisi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis molestias repudiandae autem quae obcaecati inventore doloremque, sed corporis possimus illum sunt animi. Aperiam, eum neque harum iste tenetur officiis voluptate!
+        <p class="mb-0">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, tempora fugiat architecto hic ullam ratione repellat cupiditate rem explicabo fugit, in ex provident corrupti labore ea vitae autem magnam voluptatem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem repudiandae accusamus inventore temporibus, molestiae aliquam eius, odio eveniet quia in consectetur deleniti perspiciatis nesciunt et adipisci quasi ad tenetur recusandae.
+        </p>
     </div>
 </div>
 
-<div class="row justify-content-center">
-    <div class="col-12 col-lg-6">
-        <div class="section">
-            <div class="section-header">
-                <div class="left-side">
-                    <h2 class="title">Lorem ipsum title</h2>
-                </div>
-                <div class="right-side">
-                    <button class="btn btn-info">
-                        Ver lista
-                    </button>
-                </div>
-            </div>
-
-            <div class="section-content">
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio soluta non dignissimos consequatur alias facere sed amet atque pariatur, praesentium commodi eligendi at ducimus animi culpa odio, tempore consectetur nisi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis molestias repudiandae autem quae obcaecati inventore doloremque, sed corporis possimus illum sunt animi. Aperiam, eum neque harum iste tenetur officiis voluptate!
-            </div>
-        </div>
-    </div>
-    <div class="col-12 col-lg-6">
-        <div class="section">
-            <div class="section-header">
-                <div class="left-side">
-                    <h2 class="title">Lorem ipsum title</h2>
-                </div>
-                <div class="right-side">
-                    <button class="btn btn-link">
-                        Ver outra lista
-                    </button>
-                </div>
-            </div>
-
-            <div class="section-content">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio soluta non dignissimos consequatur alias facere sed amet atque pariatur, praesentium commodi eligendi at ducimus animi culpa odio, tempore consectetur nisi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis molestias repudiandae autem quae obcaecati inventore doloremque, sed corporis possimus illum sunt animi. Aperiam, eum neque harum iste tenetur officiis voluptate!
-            </div>
-        </div>
-    </div>
-</div>
 
 <?= $v->end("content") ?>
--- source/App/Controllers/Dash/IndexController.php
+++ source/App/Controllers/Dash/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers\Dash;
 
 use App\Models\Auth;
+use App\Models\User;
 
 class IndexController extends DashController
 {
@@ -28,7 +29,35 @@ class IndexController extends DashController
      */
     public function dash(): void
     {
-        $this->view("dash/index")->seo("Resumo geral do sistema")->render();
+        $overviewBoxes = [
+            "users" => (object) [
+                "total" => (new User())->find()->count(),
+                "text" => "Usuários",
+                "icon" => icon_class("userGroup"),
+                "link" => $this->route("dash.users")
+            ],
+            "products" => (object) [
+                "total" => 0,
+                "text" => "Produtos",
+                "icon" => icon_class("box2"),
+                "link" => $this->route("dash.products")
+            ],
+            "lorem1" => (object) [
+                "total" => 0,
+                "text" => "Lorem 1",
+                "icon" => icon_class("bell"),
+                "link" => ""
+            ],
+            "lorem2" => (object) [
+                "total" => 0,
+                "text" => "Lorem 2",
+                "icon" => icon_class("sliders"),
+                "link" => ""
+            ],
+        ];
+        $this->view("dash/index", [
+            "overviewBoxes" => $overviewBoxes
+        ])->seo("Resumo geral do sistema")->render();
     }
 
     /**
