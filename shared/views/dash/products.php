eefc18a3468958b777f2c8eb2e328b07337595ea
Ernandes R Souza <ernandesrsouza@gmail.com>
2022-06-24 13:31:23 -0400

    Listando produtos cadastrados

--- shared/views/dash/products.php
+++ shared/views/dash/products.php
@@ -19,7 +19,7 @@
             <table class="table table-sm table-hover table-borderless rounded">
                 <thead class="thead-light">
                     <tr>
-                        <th>ID</th>
+                        <th class="text-center">ID</th>
                         <th>Nome</th>
                         <th>Modo de compra</th>
                         <th>Modo de venda</th>
@@ -31,19 +31,19 @@
                         /** @var \App\Models\Product $product */
                         foreach ($products as $product) : ?>
                             <tr>
-                                <td>
+                                <td class="align-middle font-weight-bold text-center">
                                     #<?= $product->id ?>
                                 </td>
-                                <td>
+                                <td class="align-middle">
                                     <?= $product->name ?>
                                 </td>
-                                <td>
+                                <td class="align-middle">
                                     <?= $product::PURCHASE_MODES_NAME[$product->purchase_mode] ?>
                                 </td>
-                                <td>
+                                <td class="align-middle">
                                     <?= $product::SALE_MODES_NAME[$product->sale_mode] ?>
                                 </td>
-                                <td>
+                                <td class="align-middle">
                                     <a class="btn btn-info" href="<?= $router->route("dash.products.edit", ["id" => $product->id]) ?>">
                                         <?= icon_elem("pencilSquare") ?>
                                     </a>
