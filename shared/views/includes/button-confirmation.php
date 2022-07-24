0da14710b5d4f0b8ab7caeb55f9c67cebefa6fd6
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-07-24 10:21:38 -0400

    Botão de confirmação

---
+++ shared/views/includes/button-confirmation.php
@@ -0,0 +1,6 @@
+<button class="btn btn-outline-<?= $btnStyle ?> <?= $btnIconClass ?? null ?> jsConfirmationModalButton"
+    data-action="<?= $btnUrlAction ?>"
+    data-style="<?= $btnStyle ?>"
+    data-message="<?= $btnMessage ?> Confirme para continuar.">
+    <?= $btnText ?>
+</button>
