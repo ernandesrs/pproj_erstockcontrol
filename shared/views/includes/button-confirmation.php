16b9b8b0f971e9a88f46120b1118206497a66dff
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-07-24 11:17:08 -0400

    btn link e btn-outline

--- shared/views/includes/button-confirmation.php
+++ shared/views/includes/button-confirmation.php
@@ -1,6 +1,3 @@
-<button class="btn btn-outline-<?= $btnStyle ?> <?= $btnIconClass ?? null ?> jsConfirmationModalButton"
-    data-action="<?= $btnUrlAction ?>"
-    data-style="<?= $btnStyle ?>"
-    data-message="<?= $btnMessage ?> Confirme para continuar.">
+<button class="btn <?= (($btnType ?? "button") == "button" ? "btn-outline-" : "btn-link text-") . $btnStyle ?> <?= $btnIconClass ?? null ?> jsConfirmationModalButton" data-action="<?= $btnUrlAction ?>" data-style="<?= $btnStyle ?>" data-message="<?= $btnMessage ?> Confirme para continuar.">
     <?= $btnText ?>
 </button>
